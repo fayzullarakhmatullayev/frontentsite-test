880cc78ef33b58c72a4d4f154758eb0cb00f48af
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-03-01 17:03:28 +0500

    Отображал все карточи из дб и добавиль лоадер

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,30 @@
   <div class="container">
     <h1 class="title">Lorem ipsum dolor sit</h1>
     <the-filter />
-    <div class="card">
+    <the-loader v-if="!buildings.length" />
+    <div class="card" v-else>
       <div class="card-item" v-for="building in buildings" :key="building.id">
         <div class="card-top">
-          <div class="card-top__flour">3 этаж</div>
+          <div class="card-top__floor">{{ building.floor }} этаж</div>
           <div class="card-top__info">
-            1 комната <span>-</span> 22.94м<sup>2</sup>
+            {{ building.rooms }} комната <span>-</span>
+            {{ building.square }}м<sup>2</sup>
           </div>
         </div>
         <div class="card-inner">
-          <div class="card-inner__id">№256</div>
-          <img class="card-inner__img" :src="building.plan" alt="image" />
+          <div class="card-inner__id">№{{ building.number }}</div>
+          <img
+            class="card-inner__img"
+            :src="building.plan"
+            :alt="building.building_name"
+          />
         </div>
         <div class="card-footer">
-          <div class="card-footer__price">2 729 860р.</div>
-          <div class="card-footer__info">119 000 р. за м<sup>2</sup></div>
+          <div class="card-footer__price">{{ prettify(building.price) }}р.</div>
+          <div class="card-footer__info">
+            {{ prettify(Math.floor(building.price / building.square)) }}р. за
+            м<sup>2</sup>
+          </div>
         </div>
         <button class="card-btn btn">ПОДРОБНЕЕ</button>
       </div>
@@ -26,15 +35,23 @@
 
 <script>
 import { mapGetters } from "vuex";
+import TheLoader from "./components/TheLoader.vue";
 import TheFilter from "./components/TheFilter.vue";
 export default {
-  components: { TheFilter },
+  components: { TheFilter, TheLoader },
   name: "App",
   data() {
     return {};
   },
   computed: {
     ...mapGetters(["buildings"]),
+  },
+  methods: {
+    prettify(num) {
+      return num
+        .toString()
+        .replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + " ");
+    },
   },
   async mounted() {
     await this.$store.dispatch("fetchData");
---
+++ src/components/TheLoader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="loader">
+    <img src="@/assets/loader.gif" alt="loader" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
